b9f787e0347525ab6b753cbf844f63e16b58592e
Abhinav <abhinavkumarjha02@gmail.com>
2025-08-31 17:09:09 +0530

    DataTypes in JS

---
+++ JavaScript/datatype.js
@@ -0,0 +1,33 @@
+console.log("hello");
+// console.log("chai");
+/*
+
+String
+Number
+Boolean
+Bigint
+
+Undefined
+null
+
+Object
+
+Symbol
+
+*/
+
+// var score = 102;
+
+let score = 102;
+let name = "abhinav"
+let isLoggedin=false;
+
+// object
+let teaTypes = ["lemon tea", "orange tea", 
+    "oolong tea"]
+
+let user = {firstname: "abhinav", lastname: "jha"}
+
+let getScore = score;
+
+console.log(getScore);
