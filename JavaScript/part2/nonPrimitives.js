2c45874d690876eb4ecd39bf46b679f40b1d7263
Abhinav <abhinavkumarjha02@gmail.com>
2025-09-16 23:44:18 +0530

    Non primitive datatypes

---
+++ JavaScript/part2/nonPrimitives.js
@@ -0,0 +1,28 @@
+const username = {
+    // firstname: "Abhinav",
+    "first name": "Abhinav",
+    isLoggedIn: true,
+};
+
+// username.firstname = "Mr. A";
+username.lastname = "jha";
+
+// console.log(username["first name"]);
+// console.log(username.firstname);
+// console.log(username.lastname);
+// console.log(username);
+// console.log(typeof username);
+
+let today = new Date();
+// console.log(today.getDate());
+
+// Array
+
+// let heros = ["a"]
+let anotherUser = ["Abhinav", true];
+// console.log(anotherUser[0]);
+
+let isValue = "2abc";
+console.log(typeof Number(isValue));
+
+
