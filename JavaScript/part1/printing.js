f62ca7b8be1cd0b0688c31ef4e49bfc31b8b5f32
Abhinav <abhinavkumarjha02@gmail.com>
2025-12-14 18:38:07 +0530

    .

--- JavaScript/part1/printing.js
+++ JavaScript/part1/printing.js
@@ -2,6 +2,7 @@ console.log("Hello");
 console.log("chai");
 // ProcessingInstruction.stdout.write("chai");
 
+console.log("Abhinav");
 
 console.table({city: "Patna"});
 console.warn({city: "Patna"});
